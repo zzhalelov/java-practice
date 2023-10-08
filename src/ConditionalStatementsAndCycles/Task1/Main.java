32df1dec1a2a98a4d642591efe0003131657bf6c
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-08 15:07:45 +0600

    fixed a bug in task 1 (it was necessary to output only even numbers)

--- src/ConditionalStatementsAndCycles/Task1/Main.java
+++ src/ConditionalStatementsAndCycles/Task1/Main.java
@@ -1,9 +1,12 @@
 package ConditionalStatementsAndCycles.Task1;
 
+//Напишите программу, которая выводит все четные числа от 1 до 10.
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
