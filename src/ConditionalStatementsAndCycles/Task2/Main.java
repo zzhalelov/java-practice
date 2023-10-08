4376e8aa1fa56926755851178a70b68f994fe051
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-08 15:19:56 +0600

    solved Task 2 (display the multiplication table by 3)

---
+++ src/ConditionalStatementsAndCycles/Task2/Main.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAndCycles.Task2;
+
+//Вывести на экран таблицу умножения на 3
+//Display the multiplication table by 3
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("3 * " + i + " = " + 3 * i);
+        }
+    }
+}
