81b6acf66b54b20bdba3d2fb0b364e05f3ba20d1
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-09 21:06:35 +0600

    solved tasks 5&6

--- src/ConditionalStatementsAndCycles/Task5/Main.java
+++ src/ConditionalStatementsAndCycles/Task5/Main.java
@@ -1,2 +1,15 @@
-package ConditionalStatementsAndCycles.Task5;public class Main {
+package ConditionalStatementsAndCycles.Task5;
+
+//Найти сумму всех чисел от 1 до 100, которые делятся на 3
+//Find the sum of all numbers from 1 to 100 that are divisible by 3
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
 }
