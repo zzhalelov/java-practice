589b2cc0e1b675795465bcac6a0b9f85637b6120
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-09 21:20:22 +0600

    modified task 7

--- src/ConditionalStatementsAndCycles/Task7/Main.java
+++ src/ConditionalStatementsAndCycles/Task7/Main.java
@@ -1,2 +1,20 @@
-package ConditionalStatementsAndCycles.Task7;public class Main {
+package ConditionalStatementsAndCycles.Task7;
+
+import java.util.Scanner;
+
+//Нужно написать программу, которая будет рассчитывать количество дней в году
+//need to write a code that will calculate the number of days in a year
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+        if (year % 400 == 0) {
+            System.out.println("Результат вычисления: 366. Год високосный");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("Результат вычисления: 366. Год вискосный");
+        } else {
+            System.out.println("Результат вычисления: 365. Год не високосный");
+        }
+    }
 }
