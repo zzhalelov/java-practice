fbb1266e8dbf83ecf23c8080dc5615e8596462a0
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-10 22:31:15 +0600

    task 1 (first generic class)

---
+++ src/generics/Task1/Box.java
@@ -0,0 +1,25 @@
+package generics.Task1;
+
+public class Box<T> {
+    private T obj;
+
+    public Box(T obj) {
+        this.obj = obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return this.obj;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "obj=" + obj +
+                "; objType=" + obj.getClass().getName() +
+                '}';
+    }
+}
