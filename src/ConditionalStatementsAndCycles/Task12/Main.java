ce32d921622bcb205d325c213505b8974bc9f0c4
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-25 19:21:24 +0600

    Output the following pattern (*) using nested loops

---
+++ src/ConditionalStatementsAndCycles/Task12/Main.java
@@ -0,0 +1,14 @@
+package ConditionalStatementsAndCycles.Task12;
+
+//Write a program to output the following pattern using nested loops
+//Напишите программу для вывода следующего узора с использованием вложенных циклов
+public class Main {
+    public static void main(String[] args) {
+        String str = "*";
+        String newStr;
+        for (int i = 1; i <= 5; i++) {
+            newStr = str.repeat(i);
+            System.out.println(newStr);
+        }
+    }
+}
