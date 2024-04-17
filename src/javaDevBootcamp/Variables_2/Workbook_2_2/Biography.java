a050a3990e26c00a94610ed537bd4f7ad3064c94
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-18 00:11:06 +0500

    Chapter 2. Variables. WorkBook 2.2

---
+++ src/javaDevBootcamp/Variables_2/Workbook_2_2/Biography.java
@@ -0,0 +1,33 @@
+package javaDevBootcamp.Variables_2.Workbook_2_2;
+
+public class Biography {
+    public static void main(String[] args) {
+
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+
+
+        // name (text)
+        String name = "John";
+        // age (whole number)
+        int age = 27;
+        // country (text)
+        String country = "USA";
+        // sport (text)
+        String sport = "baseball";
+        // hours (whole number)
+        int hours = 8;
+        // game (text)
+        String game = "chess";
+        // subject (text)
+        String subject = "geography";
+        // grade (character)
+        char grade = 'A';
+
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country);
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored a " + grade + ".");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
