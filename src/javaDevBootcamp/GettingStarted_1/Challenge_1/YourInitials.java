f36c076753a32e55a06c0857b77926dc7b79758b
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-05-02 23:03:57 +0500

    FirstProgram.java & YourInitials.java

---
+++ src/javaDevBootcamp/GettingStarted_1/Challenge_1/YourInitials.java
@@ -0,0 +1,12 @@
+package javaDevBootcamp.GettingStarted_1.Challenge_1;
+
+public class YourInitials {
+    public static void main(String[] args) {
+        // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("zzzzz  zzzzz");
+        System.out.println("   z      z ");
+        System.out.println("  z      z  ");
+        System.out.println(" z      z   ");
+        System.out.println("zzzzz  zzzzz");
+    }
+}
---
+++ src/javaDevBootcamp/GettingStarted_1/Workbook_1/FirstProgram.java
@@ -0,0 +1,9 @@
+package javaDevBootcamp.GettingStarted_1.Workbook_1;
+
+// I'm a comment. I have no impact on your code.
+// Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("I'm a comment");
+    }
+}
